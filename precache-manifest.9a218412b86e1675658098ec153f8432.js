6834dc2aef89715c07c4fdf183b415e8e1b115fa
Sandor Desi <d.alex.galaxy@gmail.com>
2020-02-11 09:19:02 +0000

    Updates

---
+++ precache-manifest.9a218412b86e1675658098ec153f8432.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "85187dae3f87479b1386",
+    "url": "/pixxie/static/css/main.30b8d618.chunk.css"
+  },
+  {
+    "revision": "85187dae3f87479b1386",
+    "url": "/pixxie/static/js/main.85187dae.chunk.js"
+  },
+  {
+    "revision": "945513008b33705d0de2",
+    "url": "/pixxie/static/js/1.94551300.chunk.js"
+  },
+  {
+    "revision": "b0778ac14c02c6805d63",
+    "url": "/pixxie/static/js/runtime~main.b0778ac1.js"
+  },
+  {
+    "revision": "53ff70a4cee965662d8ab717c011af3d",
+    "url": "/pixxie/index.html"
+  }
+];
