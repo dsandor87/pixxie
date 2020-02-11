07b9eed97ee3266e0b29f81fc68d794efbf22762
Sandor Desi <d.alex.galaxy@gmail.com>
2020-02-11 09:12:36 +0000

    first commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+    state = {term: ""};
+
+    onFormSubmit = event => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
+    };
+
+    render() {
+        return (
+            <div className="ui segment">
+                <form
+                    onSubmit={event => this.onFormSubmit(event)}
+                    className="ui form"
+                >
+                    <div className="field">
+                        <h1>Pixie</h1>
+                        <input
+                            placeholder="Search for High Quality Images"
+                            type="text"
+                            value={this.state.term}
+                            onChange={e =>
+                                this.setState({term: e.target.value})
+                            }
+                        />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
